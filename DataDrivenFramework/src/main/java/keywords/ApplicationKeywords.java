861ce7905ac2b8ca5ff316eb91a2764840e2c0a2
hsadhikari <harish.adhikari@gmail.com>
2024-08-03 23:39:05 +0530

    commiting to testkeywords addition

--- DataDrivenFramework/src/main/java/keywords/ApplicationKeywords.java
+++ DataDrivenFramework/src/main/java/keywords/ApplicationKeywords.java
@@ -10,59 +10,59 @@ import org.testng.asserts.SoftAssert;
 
 import com.aventstack.extentreports.ExtentTest;
 
-public class ApplicationKeywords extends ValidationKeywords{
-	
+public class ApplicationKeywords extends ValidationKeywords {
+// testing git 
 	public ApplicationKeywords() {
-		String path  = System.getProperty("user.dir")+"//src//test//resources//env.properties";
+		String path = System.getProperty("user.dir") + "//src//test//resources//env.properties";
 		prop = new Properties();
 		envProp = new Properties();
 		try {
 			FileInputStream fs = new FileInputStream(path);
 			prop.load(fs);
-			String env=prop.getProperty("env")+".properties";
-			path  = System.getProperty("user.dir")+"//src//test//resources//"+env;
+			String env = prop.getProperty("env") + ".properties";
+			path = System.getProperty("user.dir") + "//src//test//resources//" + env;
 			fs = new FileInputStream(path);
 			envProp.load(fs);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		softAssert = new SoftAssert();
-		
+
 	}
-	
+
 	public void login() {
-		
+
 	}
-	
+
 	public void selectDateFromCalendar(String date) {
-		log("Selecting Date "+date);
-		
+		log("Selecting Date " + date);
+
 		try {
 			Date currentDate = new Date();
-			Date dateToSel=new SimpleDateFormat("d-MM-yyyy").parse(date);
-			String day=new SimpleDateFormat("d").format(dateToSel);
-			String month=new SimpleDateFormat("MMMM").format(dateToSel);
-			String year=new SimpleDateFormat("yyyy").format(dateToSel);
-			String monthYearToBeSelected=month+" "+year;
-			String monthYearDisplayed=getElement("monthyear_css").getText();
-			
-			while(!monthYearToBeSelected.equals(monthYearDisplayed)) {
+			Date dateToSel = new SimpleDateFormat("d-MM-yyyy").parse(date);
+			String day = new SimpleDateFormat("d").format(dateToSel);
+			String month = new SimpleDateFormat("MMMM").format(dateToSel);
+			String year = new SimpleDateFormat("yyyy").format(dateToSel);
+			String monthYearToBeSelected = month + " " + year;
+			String monthYearDisplayed = getElement("monthyear_css").getText();
+
+			while (!monthYearToBeSelected.equals(monthYearDisplayed)) {
 				click("datebackButoon_xpath");
-				monthYearDisplayed=getElement("monthyear_css").getText();
+				monthYearDisplayed = getElement("monthyear_css").getText();
 			}
-			driver.findElement(By.xpath("//td[text()='"+day+"']")).click();
+			driver.findElement(By.xpath("//td[text()='" + day + "']")).click();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
-	
 	public void verifyStockAdded() {
-		
+
 	}
+
 	public void defaultLogin() {
 		navigate("url");
 		type("username_css", envProp.getProperty("admin_user_name"));
@@ -70,50 +70,57 @@ public class ApplicationKeywords extends ValidationKeywords{
 		click("login_submit_id");
 		waitForPageToLoad();
 		wait(5);
-		
+
 	}
-	
+
 	public int findCurrentStockQuantity(String companyName) {
-		log("Finding current stock quantity for "+ companyName);
-		int row = getRowNumWithCellData("stocktable_css",companyName);
-		if(row==-1) {
+		log("Finding current stock quantity for " + companyName);
+		int row = getRowNumWithCellData("stocktable_css", companyName);
+		if (row == -1) {
 			log("Current Stock Quantity is 0 as Stock not present in list");
 			return 0;
 		}
 		// table#stock > tbody > tr:nth-child(2) >td:nth-child(4)
-		String quantity = driver.findElement(By.cssSelector(prop.getProperty("stocktable_css")+" > tr:nth-child("+row+") >td:nth-child(4)")).getText();
-		log("Current stock Quantity "+quantity);
+		String quantity = driver
+				.findElement(By.cssSelector(
+						prop.getProperty("stocktable_css") + " > tr:nth-child(" + row + ") >td:nth-child(4)"))
+				.getText();
+		log("Current stock Quantity " + quantity);
 		return Integer.parseInt(quantity);
 	}
 
 	public void goToBuySell(String companyName) {
-		log("Selecting the company row "+companyName );
+		log("Selecting the company row " + companyName);
 		int row = getRowNumWithCellData("stocktable_css", companyName);
-		if(row==-1) {
+		if (row == -1) {
 			log("Stock not present in list");
 		}
-		driver.findElement(By.cssSelector(prop.getProperty("stocktable_css")+" > tr:nth-child("+row+") >td:nth-child(1)")).click();
-		driver.findElement(By.cssSelector(prop.getProperty("stocktable_css")+"  tr:nth-child("+row+") input.buySell" )).click();
-		
+		driver.findElement(
+				By.cssSelector(prop.getProperty("stocktable_css") + " > tr:nth-child(" + row + ") >td:nth-child(1)"))
+				.click();
+		driver.findElement(
+				By.cssSelector(prop.getProperty("stocktable_css") + "  tr:nth-child(" + row + ") input.buySell"))
+				.click();
+
 	}
 
 	public void goToTransactionHistory(String companyName) {
-	    log("Selecting the company row "+companyName );
+		log("Selecting the company row " + companyName);
 		int row = getRowNumWithCellData("stocktable_css", companyName);
-		if(row==-1) {
+		if (row == -1) {
 			log("Stock not present in list");
 			// report failure
 		}
-		driver.findElement(By.cssSelector(prop.getProperty("stocktable_css")+" > tr:nth-child("+row+") >td:nth-child(1)")).click();
-		driver.findElement(By.cssSelector(prop.getProperty("stocktable_css")+"  tr:nth-child("+row+") input.equityTransaction" )).click();
-		
+		driver.findElement(
+				By.cssSelector(prop.getProperty("stocktable_css") + " > tr:nth-child(" + row + ") >td:nth-child(1)"))
+				.click();
+		driver.findElement(By.cssSelector(
+				prop.getProperty("stocktable_css") + "  tr:nth-child(" + row + ") input.equityTransaction")).click();
+
 	}
 
-
-	
 	public void setReport(ExtentTest test) {
-		this.test=test;
+		this.test = test;
 	}
-
 
 }
--- DataDrivenFramework/src/main/java/keywords/GenericKeywords.java
+++ DataDrivenFramework/src/main/java/keywords/GenericKeywords.java
@@ -39,268 +39,260 @@ public class GenericKeywords {
 	public Properties envProp;
 	public ExtentTest test;
 	public SoftAssert softAssert;
-	
-	
-	
+
+//testing git
 	public void openBrowser(String browser) {
-		log("Opening The Browser "+ browser);
-		if(prop.get("grid_run").equals("Y")) {
-			
-			DesiredCapabilities cap=new DesiredCapabilities();
-			if(browser.equals("Mozilla")){
-				
+		log("Opening The Browser " + browser);
+		if (prop.get("grid_run").equals("Y")) {
+
+			DesiredCapabilities cap = new DesiredCapabilities();
+			if (browser.equals("Mozilla")) {
+
 				cap.setBrowserName("firefox");
 				cap.setJavascriptEnabled(true);
 				cap.setPlatform(org.openqa.selenium.Platform.WINDOWS);
-				
-			}else if(browser.equals("Chrome")){
-				 cap.setBrowserName("chrome");
-				 cap.setPlatform(org.openqa.selenium.Platform.WINDOWS);
+
+			} else if (browser.equals("Chrome")) {
+				cap.setBrowserName("chrome");
+				cap.setPlatform(org.openqa.selenium.Platform.WINDOWS);
 			}
-			
+
 			try {
 				// hit the hub
 				driver = new RemoteWebDriver(new URL("http://localhost:4444"), cap);
 			} catch (Exception e) {
-			  e.printStackTrace();
+				e.printStackTrace();
 			}
-		}else {  //local machine
-			if(browser.equals("Chrome")) {
+		} else { // local machine
+			if (browser.equals("Chrome")) {
 				System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
 				driver = new ChromeDriver();
-			}else if(browser.equals("Mozilla")) {
+			} else if (browser.equals("Mozilla")) {
 				System.setProperty("webdriver.gecko.driver", "D:\\Common\\drivers\\geckodriver.exe");
 				driver = new FirefoxDriver();
-			}else if(browser.equals("Edge")) {
+			} else if (browser.equals("Edge")) {
 				System.setProperty("webdriver.edge.driver", "D:\\Common\\msedgedriver.exe");
 				driver = new EdgeDriver();
 			}
 		}
 		// implicit wait
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-		//driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
-		
+		// driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
+
 	}
-	
+
 	public void navigate(String urlKey) {
-		log("Navigating to "+ urlKey);
+		log("Navigating to " + urlKey);
 		driver.get(envProp.getProperty(urlKey));
 	}
-	
+
 	public void click(String locatorKey) {
-		log("Clicking on "+locatorKey);
+		log("Clicking on " + locatorKey);
 		getElement(locatorKey).click();// not present, not visible
 	}
-	
-	public void type(String locatorKey,String data) {
-		log("Typing in "+locatorKey+" . Data "+ data);
+
+	public void type(String locatorKey, String data) {
+		log("Typing in " + locatorKey + " . Data " + data);
 		getElement(locatorKey).sendKeys(data);
 	}
+
 	public void clear(String locatorKey) {
-		log("Clearing text field "+ locatorKey);
+		log("Clearing text field " + locatorKey);
 		getElement(locatorKey).clear();
 	}
-	
+
 	public void clickEnterButton(String locatorKey) {
 		log("Clinking enter button");
 		getElement(locatorKey).sendKeys(Keys.ENTER);
 	}
-	
+
 	public void selectByVisibleText(String locatorKey, String data) {
 		Select s = new Select(getElement(locatorKey));
 		s.selectByVisibleText(data);
 	}
-	
-	
+
 	public String getText(String locatorKey) {
 		return getElement(locatorKey).getText();
 	}
-	
+
 	// central functions to extract elements
 	public WebElement getElement(String locatorKey) {
-		//  check the presence
-		if(!isElementPresent(locatorKey)) {
+		// check the presence
+		if (!isElementPresent(locatorKey)) {
 			// report failure
-			System.out.println("Element not present "+locatorKey);
+			System.out.println("Element not present " + locatorKey);
 		}
-		//  check the visibility
-		if(!isElementVisible(locatorKey)) {
+		// check the visibility
+		if (!isElementVisible(locatorKey)) {
 			// report failure
-			System.out.println("Element not visible "+locatorKey);
+			System.out.println("Element not visible " + locatorKey);
 		}
-			
+
 		WebElement e = driver.findElement(getLocator(locatorKey));
-		
+
 		return e;
 	}
-	
+
 	// true - present
 	// false - not present
 	public boolean isElementPresent(String locatorKey) {
-		log("Checking presence of "+locatorKey);
+		log("Checking presence of " + locatorKey);
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 		try {
 			wait.until(ExpectedConditions.presenceOfElementLocated(getLocator(locatorKey)));
-			
-		}catch(Exception e) {
+
+		} catch (Exception e) {
 			return false;
 		}
 		return true;
 	}
-	
+
 	// true - visible
 	// false - not visible
 	public boolean isElementVisible(String locatorKey) {
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 		try {
 			wait.until(ExpectedConditions.visibilityOfElementLocated(getLocator(locatorKey)));
-		}catch(Exception e) {
+		} catch (Exception e) {
 			return false;
 		}
 		return true;
 	}
-	
+
 	public By getLocator(String locatorKey) {
-		By by=null;
-		
-		if(locatorKey.endsWith("_id"))
+		By by = null;
+
+		if (locatorKey.endsWith("_id"))
 			by = By.id(prop.getProperty(locatorKey));
-		else if(locatorKey.endsWith("_xpath"))
+		else if (locatorKey.endsWith("_xpath"))
 			by = By.xpath(prop.getProperty(locatorKey));
-		else if(locatorKey.endsWith("_css"))
+		else if (locatorKey.endsWith("_css"))
 			by = By.cssSelector(prop.getProperty(locatorKey));
-		else if(locatorKey.endsWith("_name"))
+		else if (locatorKey.endsWith("_name"))
 			by = By.name(prop.getProperty(locatorKey));
-		
+
 		return by;
-		
-		
+
 	}
-	
-	//reporting functions
+
+	// reporting functions
 	public void log(String msg) {
 		System.out.println(msg);
 		test.log(Status.INFO, msg);
 	}
-	
+
 	public void reportFailure(String failureMsg, boolean stopOnFailure) {
 		System.out.println(failureMsg);
 		test.log(Status.FAIL, failureMsg);// failure in extent reports
 		takeScreenShot();// put the screenshot in reports
 		softAssert.fail(failureMsg);// failure in TestNG reports
-		
-		if(stopOnFailure) {
+
+		if (stopOnFailure) {
 			Reporter.getCurrentTestResult().getTestContext().setAttribute("criticalFailure", "Y");
 			assertAll();// report all the failures
 		}
 	}
-	
+
 	public void assertAll() {
 		softAssert.assertAll();
 	}
-	
-	public void takeScreenShot(){
+
+	public void takeScreenShot() {
 		// fileName of the screenshot
-		Date d=new Date();
-		String screenshotFile=d.toString().replace(":", "_").replace(" ", "_")+".png";
+		Date d = new Date();
+		String screenshotFile = d.toString().replace(":", "_").replace(" ", "_") + ".png";
 		// take screenshot
-		File srcFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		File srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
 		try {
 			// get the dynamic folder name
-			FileUtils.copyFile(srcFile, new File(ExtentManager.screenshotFolderPath+"//"+screenshotFile));
-			//put screenshot file in reports
-			test.log(Status.INFO,"Screenshot-> "+ test.addScreenCaptureFromPath(ExtentManager.screenshotFolderPath+"//"+screenshotFile));
+			FileUtils.copyFile(srcFile, new File(ExtentManager.screenshotFolderPath + "//" + screenshotFile));
+			// put screenshot file in reports
+			test.log(Status.INFO, "Screenshot-> "
+					+ test.addScreenCaptureFromPath(ExtentManager.screenshotFolderPath + "//" + screenshotFile));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
-	
-	public void waitForPageToLoad(){
-		JavascriptExecutor js = (JavascriptExecutor)driver;
-		int i=0;
+
+	public void waitForPageToLoad() {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		int i = 0;
 		// ajax status
-		while(i!=10){
-		String state = (String)js.executeScript("return document.readyState;");
-		System.out.println(state);
+		while (i != 10) {
+			String state = (String) js.executeScript("return document.readyState;");
+			System.out.println(state);
 
-		if(state.equals("complete"))
-			break;
-		else
-			wait(2);
+			if (state.equals("complete"))
+				break;
+			else
+				wait(2);
 
-		i++;
+			i++;
 		}
 		// check for jquery status
-		i=0;
-		while(i!=10){
-	
-			Long d= (Long) js.executeScript("return jQuery.active;");
+		i = 0;
+		while (i != 10) {
+
+			Long d = (Long) js.executeScript("return jQuery.active;");
 			System.out.println(d);
-			if(d.longValue() == 0 )
-			 	break;
+			if (d.longValue() == 0)
+				break;
 			else
-				 wait(2);
-			 i++;
-				
-			}
-		
+				wait(2);
+			i++;
+
 		}
-	
+
+	}
+
 	public void wait(int time) {
 		try {
-			Thread.sleep(time*1000);
+			Thread.sleep(time * 1000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
-	public void acceptAlert(){
+
+	public void acceptAlert() {
 		test.log(Status.INFO, "Switching to alert");
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 		wait.until(ExpectedConditions.alertIsPresent());
-		try{
+		try {
 			driver.switchTo().alert().accept();
 			driver.switchTo().defaultContent();
 			test.log(Status.INFO, "Alert accepted successfully");
-		}catch(Exception e){
-				reportFailure("Alert not found when mandatory",true);
+		} catch (Exception e) {
+			reportFailure("Alert not found when mandatory", true);
 		}
-		
+
 	}
 
 	// finds the row number of the data
 	public int getRowNumWithCellData(String tableLocator, String data) {
-		
+
 		WebElement table = getElement(tableLocator);
 		List<WebElement> rows = table.findElements(By.tagName("tr"));
-		for(int rNum=0;rNum<rows.size();rNum++) {
+		for (int rNum = 0; rNum < rows.size(); rNum++) {
 			WebElement row = rows.get(rNum);
 			List<WebElement> cells = row.findElements(By.tagName("td"));
-			for(int cNum=0;cNum<cells.size();cNum++) {
+			for (int cNum = 0; cNum < cells.size(); cNum++) {
 				WebElement cell = cells.get(cNum);
-				System.out.println("Text "+ cell.getText());
-				if(!cell.getText().trim().equals(""))
-					if(data.startsWith(cell.getText()))
-						return(rNum+1);
+				System.out.println("Text " + cell.getText());
+				if (!cell.getText().trim().equals(""))
+					if (data.startsWith(cell.getText()))
+						return (rNum + 1);
 			}
 		}
-		
+
 		return -1; // data is not found
 	}
-	
 
 	public void quit() {
 		driver.quit();
-		
-	}
 
-	
-	
-	
-	
-	
+	}
 
 }
